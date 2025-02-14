6772240e34c7a6ac8b56e32af90944d39885a99b
FIC <fic@info.local>
2025-02-13 21:42:45 -0300

    Exercicios de JavaScript

---
+++ sandbox/Condicionais.js
@@ -0,0 +1,67 @@
+// Exercicio 1
+//let num = prompt("Digite um numero:");
+
+//if (num > 0) {
+
+//    console.log("Positivo.");
+
+//} else if (num < 0) {
+
+//    console.log("negativo.")
+
+//} else {
+
+//    console.log("E zero.")
+
+//}
+
+//exercicio 2
+
+//let num = prompt("Digite um numero.")
+
+//if (num % 2 == 02) {
+
+//    console.log("E par.")
+
+//} else {
+
+//    console.log("E impar.")
+
+//}
+
+//exercicio 3
+
+//let num = prompt("Digite um numero.")
+
+//if (num <= 12) {
+
+//console.log("Crianca.") 
+
+//} else 
+
+//if (num >= 18) {
+
+//    console.log("Adulto.")
+
+//}
+
+//Exercicio 4
+
+//let num = prompt("Digite um numero.")
+
+//if (num <59){
+
+//    console.log("Reprovado")
+
+//} else 
+
+//if (num >= 60) {
+
+//    console.log("Aprovado")
+
+//}
+    
+//Intermediario
+//Exercicio 2
+
+
