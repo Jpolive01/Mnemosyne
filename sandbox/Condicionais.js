5a2b26d154d4131bab3713cccd4b20843018b8ab
FIC <fic@info.local>
2025-02-14 21:57:57 -0300

    Exercicios

--- sandbox/Condicionais.js
+++ sandbox/Condicionais.js
@@ -62,6 +62,111 @@
 //}
     
 //Intermediario
+
+//Exercicio 1 (Sem switch)
+
+//let num1 = prompt("Insira número")
+//let num2 = prompt("Insira número")
+//let operacao = prompt("Insira +,-,*ou/")
+//num1 = Number(num1)
+//num2 = Number(num2)
+//if (operacao == "+") {
+//    console.log(num1 + num2)
+//} else
+//if (operacao == "-") {
+//    console.log(num1 - num2)
+//} else 
+//if (operacao == "*") {
+//    console.log(num1 * num2)
+//} else 
+//if (operacao == "/") {
+//    console.log(num1 / num2)
+//} else {
+//    console.log("os dados inseridos não são aceitos")
+//}
+
+//Exercicio 1 (Com switch)
+
+//let num1 = prompt("Insira número")
+//let num2 = prompt("Insira número")
+//let operacao = prompt("Insira +,-,*ou/")
+
+//switch (operacao) {
+//    case "+":
+        
+//       let resultado = num1 + num2;
+//        console.log("resultado",resultado);
+
+//        break;
+
+//    default:
+
+//    console.log("invalido")
+
+//        break;
+//}
+
 //Exercicio 2
 
+//let num1 = prompt("Insira o número 1")
+//let num2 = prompt("Insira o número 2")
+//let num3 = prompt("Insira o número 3")
 
+//if (num1 > num2 && num1 > num3) {
+
+//    console.log(num1 + " é o maior")
+
+//}else if (num2 > num1 && num2 > num3) {
+
+//    console.log(num2 + " é o maior")
+
+//} else if (num3 > num1 && num3 > num2) {
+
+//    console.log(num3 + " é o maior")
+
+//} else {
+
+//    console.log("insira outros")
+
+//}
+
+//Exercicio 3
+
+//let num = prompt("Insira um número de 1 a 200")
+//let desconto = (num * 0.1)
+
+//let valordescontado = (num - desconto)
+
+//if (num >= 100) {
+//    console.log(valordescontado)
+//} else {
+//    console.log(num)
+//}
+
+//Exercicio 4
+
+//let num1 = prompt("Insira Usuário")
+//let num2 = prompt("Insira Senha")
+
+//if (num1 == "admin" && num2 == "1234") {
+//    console.log("Login bem sucedido")
+//} else {
+//    console.log("Acesso Negado")
+//}
+
+//Avancado
+//Exercicio 1
+let num1 = prompt("Insira Medida")
+let num2 = prompt("Insira Medida")
+let num3 = prompt("Insira Medida")
+
+if (num1 = num2 && num2 == num3 ) {
+    console.log("Triângulo Equilátero")
+} else {
+if (num1 == num2 && num1 != num3 && num2 == num3 )    
+}   
+
+
+//Exercicio 2
+//Exercicio 3
+//Exercicio 4
